c925c4a41389918f61e5ccb342f591f4b289df21
moozzyk <moozzyk@gmail.com>
2016-02-26 19:35:27 -0800

    Adding ctors

--- src/Microsoft.AspNetCore.SignalR.Client/Connection.cs
+++ src/Microsoft.AspNetCore.SignalR.Client/Connection.cs
@@ -1,10 +1,40 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using System;
 
 namespace Microsoft.AspNetCore.SignalR.Client
 {
     public class Connection
     {
+        public Connection(string url)
+            : this(url, null)
+        {
+        }
 
+        public Connection(string url, string queryString)
+        {
+            if (url == null)
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            if (url.Contains("?"))
+            {
+                throw new ArgumentException(Resources.Error_UrlCantContainQueryStringDirectly, nameof(url));
+            }
+
+            if (!url.EndsWith("/", StringComparison.Ordinal))
+            {
+                url += "/";
+            }
+
+            Url = url;
+            QueryString = queryString;
+        }
+
+
+        public string Url { get; }
+
+        public string QueryString { get; }
     }
 }
--- test/Microsoft.AspNetCore.SignalR.Client.Tests/ConnectionFacts.cs
+++ test/Microsoft.AspNetCore.SignalR.Client.Tests/ConnectionFacts.cs
@@ -1,5 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using System;
 using Xunit;
 
 namespace Microsoft.AspNetCore.SignalR.Client
@@ -7,9 +8,27 @@ namespace Microsoft.AspNetCore.SignalR.Client
     public class ConnectionFacts
     {
         [Fact]
-        public void Test()
+        public void Connection_ctor_validates_params()
         {
-            Assert.True(true);
+            Assert.Equal(
+                "url",
+                Assert.Throws<ArgumentNullException>(() => new Connection(null)).ParamName);
+
+            var ex = Assert.Throws<ArgumentException>(() => new Connection("abc?"));
+            Assert.Equal($"Url cannot contain query string directly. Pass query string values in using available overload.{Environment.NewLine}Parameter name: url", ex.Message);
+            Assert.Equal("url", ex.ParamName);
+        }
+
+        [Fact]
+        public void Connection_ctor_initializes_properties()
+        {
+            var connection = new Connection("url");
+            Assert.Equal("url/", connection.Url);
+            Assert.Null(connection.QueryString);
+
+            connection = new Connection("url", "qs");
+            Assert.Equal("url/", connection.Url);
+            Assert.Equal("qs", connection.QueryString);
         }
     }
 }
